0587a59d61f11584a94f1fc6f11ff0668c9078ba
wurong <wurong@autoxing.com>
2025-08-08 15:13:47 +0800

    add

--- flash_jenkins.py
+++ flash_jenkins.py
@@ -12,8 +12,6 @@ async def load_session(page):
 
 async def main():
     async with async_playwright() as p:
-        # 替换为你本地的 Chrome 浏览器路径
-        # 如果是linux 系统
         print(os.name)
         if os.name == 'posix':
             executable_path = "/opt/google/chrome/google-chrome"
@@ -21,19 +19,16 @@ async def main():
             executable_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
         
         # 启动浏览器并指定路径
-        browser = await p.chromium.launch(
+        context = await p.chromium.launch_persistent_context(
             executable_path=executable_path,
-            headless=False  # 如果你希望看到浏览器界面，设置为 False
+            headless=False,  # 如果你希望看到浏览器界面，设置为 False
+            # 保存用户数据
+            user_data_dir="./.cache/user_data"
         )
-
-        # 创建一个新的上下文（浏览器窗口）
-        context = await browser.new_context()
 
         # 创建一个新的页面
         page = await context.new_page()
 
-        # 尝试加载保存的 session
-        await load_session(page)
 
         ##########################################
 #         L352507606062zM
@@ -84,7 +79,7 @@ async def main():
 
 
         # 关闭浏览器
-        await browser.close()
+        await page.wait_for_event("close")
 
 # 运行异步主函数
 if __name__ == "__main__":
